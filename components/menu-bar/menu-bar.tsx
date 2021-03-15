0584cb5a36c0bab10371fe63dc948c375a00e579
Ivan <postscriptum.no@gmail.com>
2021-03-15 18:53:39 +0200

    feat: add state - is menu bar clicked

--- components/menu-bar/menu-bar.tsx
+++ components/menu-bar/menu-bar.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 const MenuBar = () => {
-  return (
-    <View style={styles.container}>
+  const [menuBarClicked, setMenuBarClicked] = useState(false);
+
+  return menuBarClicked ? (
+    <View
+      onTouchStart={() => setMenuBarClicked(!menuBarClicked)}
+    >
+      <Text style={styles.plusIcon}>&times;</Text>
+    </View>
+  ) : (
+    <View style={styles.container} onTouchStart={() => setMenuBarClicked(!menuBarClicked)}>
       <View style={styles.lineLeft}></View>
       <View style={styles.lineRight}></View>
       <View style={styles.lineLeft}></View>
@@ -16,7 +24,7 @@ const styles = StyleSheet.create({
     width: 32,
     height: 24,
     flexDirection: 'column',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
   },
   lineLeft: {
     width: 25,
@@ -34,7 +42,11 @@ const styles = StyleSheet.create({
     borderColor: '#fff',
     borderRadius: 20,
     backgroundColor: '#fff'
-  }
+  },
+  plusIcon: {
+    fontSize: 50,
+    color: '#fff'
+  },
 });
 
 export default MenuBar;
