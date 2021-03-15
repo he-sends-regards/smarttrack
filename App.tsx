b7fde7b93100200675741ecfa0b29b962d886417
Ivan <postscriptum.no@gmail.com>
2021-03-15 18:53:03 +0200

    fix: wrap app into SafeAreaNetwork

--- App.tsx
+++ App.tsx
@@ -1,29 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, SafeAreaView, View } from 'react-native';
 import Menu from './components/menu/menu';
 import Navbar from './components/navbar/navbar';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Navbar />
+
       <View>
         <Menu />
       </View>
-      <Text>Open up App.tsx to start working on your app!</Text>
+
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 45,
-    paddingRight: 16,
     backgroundColor: '#fff',
     alignItems: 'flex-end',
-    justifyContent: 'flex-start',
+    justifyContent: 'space-between',
   },
 });
