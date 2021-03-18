54d16c0c16e44e3d24522cc4e028dfb2f55dbfbe
Ivan <postscriptum.no@gmail.com>
2021-03-18 21:46:38 +0200

    feat: create stuff menu

--- screens/stuff/stuff.tsx
+++ screens/stuff/stuff.tsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import {SafeAreaView} from "react-native";
 
 import Navbar from "../../components/navbar/navbar";
+import StuffList from "../../components/stuff-list/stuff-list";
+
+const listItems = ["Doctors", "Assistans", "Receptionist"];
 
 const Stuff = () => {
+  const [activeListItem, setActiveListItem] = useState("Doctors");
+
   return (
     <SafeAreaView>
       <Navbar />
+      <StuffList
+        activeListItem={activeListItem}
+        setActiveListItem={setActiveListItem}
+        listItems={listItems}
+      />
     </SafeAreaView>
   );
 };
