e5c2cf4afce0987bf0008ac8c7b2a9aa6ddf25bc
Dmytro <sosnikova@gmail.com>
2021-03-19 18:03:13 +0200

    feat: button in work

--- screens/allerts/allerts.tsx
+++ screens/allerts/allerts.tsx
@@ -1,12 +1,38 @@
 import React from "react";
 import {SafeAreaView} from "react-native";
 
+import Button from "../../components/buttons/button";
 import Navbar from "../../components/navbar/navbar";
+import {primaryColor, dangerColor} from "../../const";
+
+const noop = () => {};
 
 const Allerts = () => {
   return (
     <SafeAreaView>
       <Navbar />
+      <Button
+        title="Add new"
+        onPress={noop}
+        backgroundColor={primaryColor}
+        width="100%"
+        color="#fff"
+      />
+      <Button
+        title="Add new"
+        onPress={noop}
+        backgroundColor="transparent"
+        width="100%"
+        color={primaryColor}
+      />
+      <Button
+        title="Add new"
+        onPress={noop}
+        backgroundColor="transparent"
+        width="25%"
+        color={dangerColor}
+        // border="4px solid {primaryColor}"
+      />
     </SafeAreaView>
   );
 };
