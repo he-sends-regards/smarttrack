43d9a25b7c5abe56bea643e07010ec1219d322d3
Ivan <postscriptum.no@gmail.com>
2021-03-23 11:10:11 +0200

    fix: solve conflicts

--- src/screens/alerts/alerts.tsx
+++ src/screens/alerts/alerts.tsx
@@ -1,13 +1,15 @@
 import React, {useState} from "react";
-import {SafeAreaView} from "react-native";
+import {StyleSheet, SafeAreaView} from "react-native";
 import {useSelector} from "react-redux";
 
 import AlertList from "../../components/alert-list/alert-list";
 import Button from "../../components/buttons/button";
 import Navbar from "../../components/navbar/navbar";
-import {Color} from "../../const";
+import StuffMenu from "../../components/stuff-menu/stuff-menu";
+import {primaryColor} from "../../const";
 import {RootState} from "../../store/store";
 
+const listItems = ["doctors", "assistans", "receptionist"];
 const noop = () => {};
 
 const Alerts = () => {
@@ -15,12 +17,17 @@ const Alerts = () => {
   const alertsData = useSelector((state: RootState) => state.alerts);
 
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <Navbar />
+      <StuffMenu
+        activeListItem={activeListItem}
+        setActiveListItem={setActiveListItem}
+        listItems={listItems}
+      />
       <Button
         title="Add new"
         onPress={noop}
-        backgroundColor={Color.primaryColor}
+        backgroundColor={primaryColor}
         width="100%"
         color="#fff"
       />
@@ -28,5 +35,12 @@ const Alerts = () => {
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#E5E5E5",
+  },
+});
 
 export default Alerts;
