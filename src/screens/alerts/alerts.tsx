bfa16e83705ef78afff368bf11fd8a9eb2ab89d7
Dmytro <sosnikova@gmail.com>
2021-03-23 16:07:38 +0200

    feat: modal for alerts in work with mock data

--- src/screens/alerts/alerts.tsx
+++ src/screens/alerts/alerts.tsx
@@ -4,21 +4,30 @@ import {useSelector} from "react-redux";
 
 import AlertList from "../../components/alert-list/alert-list";
 import Button from "../../components/buttons/button";
+import AlertModal from "../../components/modal/alert-modal";
 import Navbar from "../../components/navbar/navbar";
 import StuffMenu from "../../components/stuff-menu/stuff-menu";
 import {primaryColor} from "../../const";
 import {RootState} from "../../store/store";
 
 const listItems = ["doctors", "assistans", "receptionist"];
-const noop = () => {};
 
 const Alerts = () => {
   const [activeListItem, setActiveListItem] = useState("doctors");
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const modalHadler = () => {
+    setModalVisible(!modalVisible);
+  };
+
   const alertsData = useSelector((state: RootState) => state.alerts);
 
   return (
     <SafeAreaView style={styles.container}>
       <Navbar />
+      {modalVisible && (
+        <AlertModal visible={modalVisible} onPress={modalHadler} />
+      )}
       <StuffMenu
         activeListItem={activeListItem}
         setActiveListItem={setActiveListItem}
@@ -26,7 +35,7 @@ const Alerts = () => {
       />
       <Button
         title="Add new"
-        onPress={noop}
+        onPress={() => setModalVisible(true)}
         backgroundColor={primaryColor}
         width="100%"
         color="#fff"
